5883b35901751e61dbf63ea6f447ff243b8d0f8d
JDgoldvox <voxcastjdgoldvox@gmail.com>
2026-07-24 13:57:22 +0100

    add filter layout , and title to app

---
+++ frontend/GreenhouseGasEmissions/src/components/Title.tsx
@@ -0,0 +1,11 @@
+﻿import style from "./Title.module.css"
+import generalStyle from "../General.module.css"
+
+export default function Title()
+{
+    return (
+        <div className = {style.title}>
+            <h1 className = {generalStyle.center}> Environmental Accounts </h1>
+        </div>
+    )
+}
---
+++ frontend/GreenhouseGasEmissions/src/components/filter/Filter.tsx
@@ -0,0 +1,33 @@
+﻿import style from "./Filter.module.css"
+
+export default function Filter()
+{
+    return(
+        <div>
+            <label className = {style.label}> Reporting Frequency
+                <select onChange={(e) => console.log(e.target.value)}>
+                    <option value="someOption">Some option</option>
+                    <option value="otherOption">Other option</option>
+                    <option value="someOption">Some option</option>
+                </select>
+            </label>
+            
+            <label className = {style.label}> Air pollutant
+                <select onChange={(e) => console.log(e.target.value)}>
+                    <option value="someOption">Some option</option>
+                    <option value="otherOption">Other option</option>
+                </select>
+            </label>
+
+            <label className = {style.label}> Sectors
+                <select>
+                    <option><input type="checkbox"/>First checkbox</option>
+                    <option><input type="checkbox"/>Second checkbox </option>
+                    <option><input type="checkbox"/>Third checkbox</option>
+                </select>
+            </label>
+
+
+        </div>
+    )
+}
