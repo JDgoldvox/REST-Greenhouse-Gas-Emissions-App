5ed4059acc96d97b0cf599cf48ed85b7359a5e78
JDgoldvox <voxcastjdgoldvox@gmail.com>
2026-07-28 02:28:08 +0100

    finish table data service and display data in table

--- frontend/GreenhouseGasEmissions/src/Services/TableDataService.ts
+++ frontend/GreenhouseGasEmissions/src/Services/TableDataService.ts
@@ -9,7 +9,9 @@ export async function TableDataService(url : string | null) {
     
     try {
         const betterUrl2 = "https://data.un.org/ws/rest/data/ESTAT,DF_SEEA_AEA,1.3/.AU+CA+NZ......GHG..../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD"
-        return await FetchTableData(betterUrl2);
+        const rawData= await FetchTableData(betterUrl2);
+        const rowData= await CleanRawData(rawData);
+        return rowData;
     }
     catch (error) {
         console.error("Error fetching data:", error);
@@ -22,5 +24,81 @@ async function FetchTableData(url : string)
     await parser.getDatasets(url);
     console.log(parser.getAnnotations());
     return parser.getData(); // returns a simplified array of observations with dimension and attributes values
+}
+
+async function CleanRawData(rawData: any[]) : Promise<any[]>
+{
+    let tableRows : any[] = [];
+    let earliestYear : number = -1;
+    let latestYear : number = -1;
+    let countryData : Map<string, Map<number, any>> = new Map<string, Map<number, any>>();
+    
+    rawData?.forEach( (item) => {
+        const key = item.REF_AREA;
+        const year : number = Number(item.TIME_PERIOD);
+
+        //check and set if earliest year so far
+        if(earliestYear === -1 || year < earliestYear)  earliestYear = year;
+        
+        //check and set if latest year so far
+        if(latestYear === -1 || year > latestYear) latestYear = year;
+
+        // Add country to map
+        if (!countryData.has(key)) {
+            countryData.set(key, new Map<number, any>());
+        }
+
+        // Add year to country data
+        countryData.get(key)?.set(year, [item]);
+    })
+
+    //console.log(countryData);
+
+    //get unique country names, and set table headers
+    {
+        let tableHeader : string[] = ["Year"];
+        countryData.forEach((_ , key) =>{
+            tableHeader.push(key);
+        })
+
+        //add to row data as first item
+        tableRows.push(tableHeader);
+    }
+    
+
+    //loop over each year that has data
+    for(let i: number = earliestYear; i <= latestYear; i++)
+    {
+        let existingData : string[] = [];
+
+        //find which country data exists for this year
+        countryData.forEach((dataMap) =>
+        {
+            //search whether year exists
+            const newData = dataMap.get(i);
+            if(newData != undefined) {
+                existingData.push(newData[0].value);
+            }
+            else
+            {
+                existingData.push("-");
+            }
+        })
+
+        //if any data exists for this year, add it to the table rows
+        let row: any[] = [];
+        row.push(i);
+        existingData.forEach((data) =>
+        {
+            row.push(data);
+        })
+
+        //add row to table rows
+        tableRows.push(row);
+    }
+
+    //set row data
+    console.log(tableRows);
+    return tableRows;
 }
 
--- frontend/GreenhouseGasEmissions/src/components/table/Table.tsx
+++ frontend/GreenhouseGasEmissions/src/components/table/Table.tsx
@@ -9,92 +9,22 @@ interface TableProps{
 
 export default function Table({url} : TableProps)
 {
-    const [tableHeader, setTableHeader] = useState<string[]>(["Year"]);
+    const [tableHeader, setTableHeader] = useState<string[]>([]);
     const [tableRow, setTableRow] = useState<any[]>([]);
     
     useEffect(() =>
     {
-        let earliestYear : number = -1;
-        let latestYear : number = -1;
-        
         //fetch data from url
         async function fetchData()
         {
-            const result = await TableDataService(url);
-            let countryData : Map<string, Map<number, any>> = new Map<string, Map<number, any>>();
-            
-            result?.forEach( (item) => {
-                const key = item.REF_AREA;
-                const year : number = Number(item.TIME_PERIOD);
-                
-                //check and set if earliest year so far
-                if(earliestYear === -1 || year < earliestYear)
-                {
-                    earliestYear = year;
-                }
-                
-                //check and set if latest year so far
-                if(latestYear === -1 || year > latestYear)
-                {
-                    latestYear = year;
-                }
-                
-                // Add country to map
-                if (!countryData.has(key)) {
-                    countryData.set(key, new Map<number, any>());
-                }
-                
-                // Add year to country data
-                countryData.get(key)?.set(year, [item]);
-            })
-            
-            console.log(countryData);
-            
-            //get unique country names
-            countryData.forEach((_ , key) =>{
-                tableHeader.push(key);
-            })
-            
-            //loop over each year that has data
-            let tempRows : any[] = [];
-            for(let i: number = earliestYear; i <= latestYear; i++)
-            {
-                let existingData : string[] = [];
-                
-                //find which country data exists for this year
-                countryData.forEach((dataMap) =>
-                {
-                    //search whether year exists
-                    const newData = dataMap.get(i);
-                    if(newData != undefined) {
-                        existingData.push(newData[0].value);
-                    }
-                    else
-                    {
-                        existingData.push("-");
-                    }
-                })
-                
-                //if any data exists for this year, add it to the table rows
-                let row: any[] = [];
-                row.push(i);
-                existingData.forEach((data) =>
-                {
-                    row.push(data);
-                })
-                
-                //add row to table rows
-                tempRows.push(row);
-            }
-            
-            //set row data
-            setTableRow(tempRows);
-            console.log(tempRows);
+             const rowData = await TableDataService(url);
+             if(rowData != null) {
+                 setTableRow(rowData);
+                 setTableHeader(rowData[0]);
+             }
         }
 
         fetchData();
-        
-        console.log("url changed");
     },[url])
     
     return (
@@ -112,35 +42,29 @@ export default function Table({url} : TableProps)
                 </tr>
             </thead>
             <tbody>
-            
-                <tr>
-                    <td>1999</td>
-                    <td>112314</td>
-                    <td>134531</td>
-                </tr>
-                <tr>
-                    <td>2000</td>
-                    <td>135135135</td>
-                    <td>135135153</td>
-                </tr>
-                <tr>
-                    <td>2001</td>
-                    <td>135135</td>
-                    <td>135315</td>
-                </tr>
-                <tr>
-                    <td>2002</td>
-                    <td>135531135</td>
-                    <td>13513135</td>
-                </tr>
+            {
+                tableRow.map((row: any[], index) =>
+                {
+                    if(index === 0) return;
+                    return (
+                        <>
+                            <tr>
+                                {
+                                    row.map((data) => {
+                                        return (
+                                            <>
+                                                <td>{data}</td>
+                                            </>
+                                        )
+                                    })
+                                }
+                                
+                            </tr>
+                        </>
+                    );
+                })
+            }
             </tbody>
-            {/*<tfoot>*/}
-            {/*    <tr>*/}
-            {/*        <td>Overall Total</td>*/}
-            {/*        <td>---</td>*/}
-            {/*        <td>---</td>*/}
-            {/*    </tr>*/}
-            {/*</tfoot>*/}
         </table>
     )
 }
